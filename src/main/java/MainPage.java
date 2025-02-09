64cd6b32dbc33292b9e32e8d72b6fbfbdc8b097f
Stanislau Schastny <stasschastny@gmail.com>
2025-02-09 20:37:41 +0100

    Add lesson_18

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,9 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
 
 public class MainPage {
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,11 +1,3 @@
 package tests;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
 public class LoginTest {
 }
